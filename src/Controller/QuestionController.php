86f0400b434b25ba00e1f1efdc43520d368383b9
luism3434 <luisrodrigomorales6@gmail.com>
2022-10-02 09:49:16 -0600

    Part 2

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Symfony\Contracts\Cache\CacheInterface;
 
 class QuestionController extends AbstractController
 {
@@ -24,7 +26,7 @@ class QuestionController extends AbstractController
     }
 
     #[Route("/questions/{slug}", name:"app_question_show")]
-    public function show($slug)
+    public function show($slug, MarkdownParserInterface $markdownParser, CacheInterface $cache)
     {
         $answers = [
             'Make sure your cat is sitting purrrfectly still 🤣',
@@ -33,11 +35,14 @@ class QuestionController extends AbstractController
         ];
 
         $questionText = "I've been turned into a cat, any thoughts on how to turn back? While I'm **adorable**, I don't really care for cat food.";
+        $parsedQuestionText = $cache->get('markdown_'.md5($questionText), function() use ($questionText, $markdownParser) {
+            return $markdownParser->transformMarkdown($questionText);
+        });
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
             'answers' => $answers,
-            'questionText' => $questionText
+            'questionText' => $parsedQuestionText
         ]);
     }
 }
